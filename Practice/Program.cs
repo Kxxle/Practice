d67a7c0b5dcb2055c377efdd6d6b9a5b4e578acc
Kxxle <64878567+Kxxle@users.noreply.github.com>
2023-05-31 17:59:12 +0200

    Add project files.

---
+++ Practice/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = 123;
+            int y = 321;
+            int z = x + y;
+            Console.WriteLine("Enter your name\n");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your height\n");
+            double height = ;
+            bool alive = false;
+            char symbol = '@';
+            String name = "Khule";
+            String[] person = {name,Convert.ToString(height),};
+            for (int i = 0; i < person.Length ; i++) { }
+
+            Console.WriteLine("Hello "+ name);
+            Console.WriteLine("Your age is"+age); 
+            Console.WriteLine("Your")
+
+            
+
+
+        }
+    }
+}
